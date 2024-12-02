ada6caa71edfb6e0c3fae71edd50be2b2952c389
Admin <Admin@DESKTOP-UB0CM36>
2024-12-02 21:31:24 +0800

    Add project files.

---
+++ GLUCOCHECK/Dashboard.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New DataVisualization.Charting.Series()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Dashboard))
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        Button4 = New Button()
+        Chart1 = New DataVisualization.Charting.Chart()
+        DateTimePicker1 = New DateTimePicker()
+        TextBox2 = New TextBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Chart1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(35, 228)
+        PictureBox1.Margin = New Padding(3, 4, 3, 4)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(54, 55)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.ForeColor = SystemColors.Control
+        Label1.Location = New Point(96, 263)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(213, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Welcome to Dashboard, Name"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(0), CByte(64), CByte(64))
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.ForeColor = SystemColors.Control
+        Button1.Location = New Point(35, 307)
+        Button1.Margin = New Padding(3, 4, 3, 4)
+        Button1.Name = "Button1"
+        Button1.RightToLeft = RightToLeft.Yes
+        Button1.Size = New Size(208, 49)
+        Button1.TabIndex = 2
+        Button1.Text = "Profile"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(0), CByte(64), CByte(64))
+        Button2.FlatStyle = FlatStyle.Popup
+        Button2.ForeColor = SystemColors.Control
+        Button2.Location = New Point(35, 364)
+        Button2.Margin = New Padding(3, 4, 3, 4)
+        Button2.Name = "Button2"
+        Button2.RightToLeft = RightToLeft.Yes
+        Button2.Size = New Size(208, 49)
+        Button2.TabIndex = 3
+        Button2.Text = "Records"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(0), CByte(64), CByte(64))
+        Button3.FlatStyle = FlatStyle.Popup
+        Button3.ForeColor = SystemColors.Control
+        Button3.Location = New Point(35, 421)
+        Button3.Margin = New Padding(3, 4, 3, 4)
+        Button3.Name = "Button3"
+        Button3.RightToLeft = RightToLeft.Yes
+        Button3.Size = New Size(208, 49)
+        Button3.TabIndex = 4
+        Button3.Text = "Signout"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(461, 323)
+        TextBox1.Margin = New Padding(3, 4, 3, 4)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(226, 27)
+        TextBox1.TabIndex = 5
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.FromArgb(CByte(0), CByte(64), CByte(64))
+        Button4.FlatStyle = FlatStyle.Popup
+        Button4.ForeColor = SystemColors.Control
+        Button4.Location = New Point(521, 435)
+        Button4.Margin = New Padding(3, 4, 3, 4)
+        Button4.Name = "Button4"
+        Button4.RightToLeft = RightToLeft.Yes
+        Button4.Size = New Size(96, 36)
+        Button4.TabIndex = 6
+        Button4.Text = "Enter"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Chart1
+        ' 
+        ChartArea1.Name = "ChartArea1"
+        Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Chart1.Legends.Add(Legend1)
+        Chart1.Location = New Point(14, 19)
+        Chart1.Margin = New Padding(3, 4, 3, 4)
+        Chart1.Name = "Chart1"
+        Chart1.Palette = DataVisualization.Charting.ChartColorPalette.Bright
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Blood Sugar Level Summary"
+        Chart1.Series.Add(Series1)
+        Chart1.Size = New Size(704, 201)
+        Chart1.TabIndex = 7
+        Chart1.Text = "Chart1"
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(461, 359)
+        DateTimePicker1.Margin = New Padding(3, 4, 3, 4)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(226, 27)
+        DateTimePicker1.TabIndex = 8
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(461, 395)
+        TextBox2.Margin = New Padding(3, 4, 3, 4)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(226, 27)
+        TextBox2.TabIndex = 9
+        ' 
+        ' Dashboard
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(731, 492)
+        Controls.Add(TextBox2)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(Chart1)
+        Controls.Add(Button4)
+        Controls.Add(TextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "Dashboard"
+        Text = "Form5"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(Chart1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents TextBox2 As TextBox
+End Class
